97d797e9aa154033ca5d014b4da4da6660d5f477
TKais <TiffanyTKaiser@gmail.com>
2014-12-08 11:51:50 -0800

    Refactoring procedural JavaScript to MVC -- starting with document.ready for controller

--- bank.js
+++ bank.js
@@ -1,10 +1,23 @@
+/////////////////PROCEDURAL///////////////////////////////
+
+// $(document).ready(function(){
+//   $("#pinForm").submit(function(){
+//     var pinInput = $('#pin').val();
+//     if(pinInput == 2242){
+//       alert('Correct PIN');
+//     } else {
+//       alert('Incorrect PIN');
+//     }
+//   })
+// });
+
+
+
+/////////////CONTROLLER///////////////////////////////////
+
 $(document).ready(function(){
-  $("#pinForm").submit(function(){
-    var pinInput = $('#pin').val();
-    if(pinInput == 2242){
-      alert('Correct PIN');
-    } else {
-      alert('Incorrect PIN');
-    }
-  })
-});
+  var view = new View();
+  var pin = new Pin();
+  var controller = new Controller(view, pin);
+  controller.bindListeners();
+})
